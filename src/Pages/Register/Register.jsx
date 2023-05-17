0713e76a57a9a30caae74b4c4d32e55a6c6c6333
Fahim Shariar <fahimshariar28@gmail.com>
2023-05-17 21:09:49 +0600

    Some setup

---
+++ src/Pages/Register/Register.jsx
@@ -0,0 +1,9 @@
+const Register = () => {
+  return (
+    <div>
+      <h1 className="text-3xl">Register</h1>
+    </div>
+  );
+};
+
+export default Register;
---
+++ src/Routes/MainRoute.jsx
@@ -0,0 +1,33 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../Pages/Home/Home";
+import MainLayout from "../Layout/MainLayout";
+import Blog from "../Pages/Blog/Blog";
+import Login from "../Pages/Login/Login";
+import Register from "../Pages/Register/Register";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout></MainLayout>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/blog",
+        element: <Blog></Blog>,
+      },
+      {
+        path: "/login",
+        element: <Login></Login>,
+      },
+      {
+        path: "/register",
+        element: <Register></Register>,
+      },
+    ],
+  },
+]);
+
+export default router;
