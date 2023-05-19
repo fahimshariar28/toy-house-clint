86b63c7129344fa81d872d4198939c34515f39cf
Fahim Shariar <fahimshariar28@gmail.com>
2023-05-20 00:50:21 +0600

    Gallery section added

--- src/Pages/Home/Gallery.jsx
+++ src/Pages/Home/Gallery.jsx
@@ -1,78 +1,48 @@
-const Gallery = () => {
+import "@smastrom/react-rating/style.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
+const GallerySection = () => {
+  AOS.init();
+
   return (
-    <div className="mt-5">
-      <h2 className="text-3xl text-primary font-bold text-center">
-        Toy Gallery
-      </h2>
-      <div className="mt-5 flex justify-center">
-        <div className="mockup-phone border-primary">
-          <div className="camera"></div>
-          <div className="display">
-            <div className="artboard artboard-demo phone-1">
-              <div className="carousel w-full">
-                <div id="slide1" className="carousel-item relative w-full">
-                  <img
-                    src="https://i.ibb.co/QM5qBJd/robot.jpg"
-                    className="w-full"
-                  />
-                  <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                    <a href="#slide4" className="btn btn-circle">
-                      ❮
-                    </a>
-                    <a href="#slide2" className="btn btn-circle">
-                      ❯
-                    </a>
-                  </div>
-                </div>
-                <div id="slide2" className="carousel-item relative w-full">
-                  <img
-                    src="https://i.ibb.co/q1dv6Y4/car.jpg"
-                    className="w-full"
-                  />
-                  <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                    <a href="#slide1" className="btn btn-circle">
-                      ❮
-                    </a>
-                    <a href="#slide3" className="btn btn-circle">
-                      ❯
-                    </a>
-                  </div>
-                </div>
-                <div id="slide3" className="carousel-item relative w-full">
-                  <img
-                    src="https://i.ibb.co/wzq6Kzn/download.jpg"
-                    className="w-full"
-                  />
-                  <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                    <a href="#slide2" className="btn btn-circle">
-                      ❮
-                    </a>
-                    <a href="#slide4" className="btn btn-circle">
-                      ❯
-                    </a>
-                  </div>
-                </div>
-                <div id="slide4" className="carousel-item relative w-full">
-                  <img
-                    src="https://i.ibb.co/Jq8f6KS/images-1.jpg"
-                    className="w-full"
-                  />
-                  <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                    <a href="#slide3" className="btn btn-circle">
-                      ❮
-                    </a>
-                    <a href="#slide1" className="btn btn-circle">
-                      ❯
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+    <section className="my-5">
+      <div className="my-container mx-auto">
+        <h1 className="text-3xl font-bold text-center my-5" data-aos="fade-up">
+          Toys Gallery
+        </h1>
+        <div
+          className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8"
+          data-aos="fade-up"
+        >
+          <img
+            src="https://i.ibb.co/VSPjdMc/red-racer.jpg"
+            className="w-full h-48 object-cover transition duration-500 ease-in-out transform hover:scale-105"
+            alt=""
+          />
+          <img
+            src="https://i.ibb.co/nC7LDPv/blue-car.jpg"
+            className="w-full h-48 object-cover transition duration-500 ease-in-out transform hover:scale-105"
+            alt=""
+          />
+          <img
+            src="https://i.ibb.co/1dByLrV/monster-truck.png"
+            className="w-full h-48 object-cover transition duration-500 ease-in-out transform hover:scale-105"
+            alt=""
+          />
+          <img
+            src="https://i.ibb.co/hstmX3K/police-car.png"
+            className="w-full h-48 object-cover transition duration-500 ease-in-out transform hover:scale-105"
+            alt=""
+          />
+          <img
+            src="https://i.ibb.co/Dpqt0wp/Convertible-Cruiser-car.jpg"
+            className="w-full h-48 object-cover transition duration-500 ease-in-out transform hover:scale-105"
+            alt=""
+          />
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
-export default Gallery;
+export default GallerySection;
