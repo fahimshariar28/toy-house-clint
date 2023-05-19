6ca070488aaf6dd29165efa4bc8b10840104201f
Fahim Shariar <fahimshariar28@gmail.com>
2023-05-19 19:03:37 +0600

    Loader added

--- src/Pages/AllToys/AllToys.jsx
+++ src/Pages/AllToys/AllToys.jsx
@@ -4,16 +4,46 @@ import SingleToy from "./SIngleToy";
 const AllToys = () => {
   const [toys, setToys] = useState([]);
   const [seeAll, setSeeAll] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
+    setLoading(true);
     fetch("http://localhost:5000/toys")
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
         setToys(data);
+        setLoading(false);
       })
       .catch((error) => console.log(error));
   }, []);
+  if (loading) {
+    return (
+      <div className="flex my-10 justify-center items-center space-x-2 text-primary">
+        <svg
+          className="w-6 h-6 animate-spin"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <circle
+            className="opacity-25"
+            cx="12"
+            cy="12"
+            r="10"
+            stroke="currentColor"
+            strokeWidth="4"
+          ></circle>
+          <path
+            className="opacity-75"
+            fill="currentColor"
+            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+          ></path>
+        </svg>
+        <span>Loading...</span>
+      </div>
+    );
+  }
   return (
     <div className="w-9/12 mx-auto">
       <h1 className="text-2xl text-primary text-center font-bold mt-5">
